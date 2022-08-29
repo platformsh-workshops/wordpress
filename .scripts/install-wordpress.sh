2bf5b6f0637c02c5c6ab118398972e6494d1f6b0
chadcarlson <chad.carlson@platform.sh>
2022-08-29 11:19:53 -0400

    Cleanup install script.

--- .scripts/install-wordpress.sh
+++ .scripts/install-wordpress.sh
@@ -1,23 +1,3 @@
-#!/usr/bin/env bash
-
-# if [[ -z "${PLATFORM_PROJECT_ENTROPY}" ]]; then
-#     printf "\nNot on a Platform.sh environment.\n"
-#     # Install WordPress
-#     ddev wp core install --url="${WP_SITEURL}" --title="Platform.sh Workshop" --admin_user="admin" --admin_password="Admin1234" --admin_email="admin@example.com" --path='wordpress'
-
-#     # Set permalink structure
-#     ddev wp option update "permalink_structure" "/%year%/%monthnum%/%postname%/" --path='wordpress'
-#     ddev wp rewrite flush --path='wordpress'
-# else
-#     printf "\nOn a Platform.sh environment.\n"
-#     # Install WordPress
-#     wp core install --url="${WP_SITEURL}" --title="Platform.sh Workshop" --admin_user="admin" --admin_password="Admin1234" --admin_email="admin@example.com" --path='wordpress'
-
-#     # Set permalink structure
-#     wp option update "permalink_structure" "/%year%/%monthnum%/%postname%/" --path='wordpress'
-#     wp rewrite flush --path='wordpress'
-# fi
-
 if [[ -z "${PLATFORM_PROJECT_ENTROPY}" ]]; then
     printf "\nNot on a Platform.sh environment.\n"
     # Install WordPress
@@ -29,6 +9,7 @@ if [[ -z "${PLATFORM_PROJECT_ENTROPY}" ]]; then
 else
     printf "\nOn a Platform.sh environment.\n"
     # Install WordPress
+    # sendmail: /etc/msmtprc: line 6: command host needs an argument
     wp core install --url="${WP_SITEURL}" --title="Platform.sh Workshop" --admin_user="admin" --admin_password="Admin1234" --admin_email="admin@example.com"
 
     # Set permalink structure
